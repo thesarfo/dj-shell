9497500e1621c2956cc8ce2f62effe2ca2ec1cb7
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-01 17:36:46 +0000

    Add custom user model, DRF serializers and views, user registration, login, logout endpoints, and initial configs

--- drf-user_model.ps1
+++ drf-user_model.ps1
@@ -117,3 +117,250 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_USER_MODEL = 'users.ApplicationUser'
+
+DJANGO_SUPERUSER_PASSWORD = 'batman29'
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'@ | Out-File -FilePath .\config\settings.py -Encoding utf8
+
+@'
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import ApplicationUserCreationForm, ApplicationUserChangeForm
+from .models import ApplicationUser
+
+
+@admin.register(ApplicationUser)
+class ApplicationUserAdmin(UserAdmin):
+    add_form = ApplicationUserCreationForm
+    form = ApplicationUserChangeForm
+    model = ApplicationUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2', 'is_staff',
+                'is_active', 'groups', 'user_permissions'
+            )}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+'@ | Out-File -FilePath .\users\admin.py -Encoding utf8
+
+@'
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import ApplicationUser
+
+
+class ApplicationUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = ApplicationUser
+        fields = ('email',)
+
+
+class ApplicationUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = ApplicationUser
+        fields = ('email',)
+
+'@ | Out-File -FilePath .\users\forms.py -Encoding utf8
+
+@'
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class ApplicationUserManager(BaseUserManager):
+    '''
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    '''
+    def create_user(self, email, password, **extra_fields):
+        '''
+        Create and save a user with the given email and password.
+        '''
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        '''
+        Create and save a SuperUser with the given email and password.
+        '''
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+'@ | Out-File -FilePath .\users\managers.py -Encoding utf8
+
+@'
+from rest_framework import serializers
+from .models import ApplicationUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplicationUser
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = ApplicationUser(
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+'@ | Out-File -FilePath .\users\serializers.py -Encoding utf8
+
+@'
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('create/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+]
+
+'@ | Out-File -FilePath .\users\urls.py -Encoding utf8
+
+@'
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.authentication import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import views
+
+from .models import ApplicationUser
+from .serializers import UserSerializer
+
+
+class Register(views.APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Login(views.APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = None
+        if '@' in email:
+            try:
+                user = ApplicationUser.objects.get(email=email)
+            except ObjectDoesNotExist:
+                print('invalid email')
+
+        if not user:
+            user = authenticate(email=email, password=password)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class Logout(views.APIView):
+    def get(self, request, format=None):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+'@ | Out-File -FilePath .\users\views.py -Encoding utf8
+
+@'
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+]
+
+'@ | Out-File -FilePath .\config\urls.py -Encoding utf8
+
+Write-Output 'Migrations are coming...'
