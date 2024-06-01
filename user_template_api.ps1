e37dd6d1a44e846ec9835c22f6750b44277cf6a3
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-01 17:42:13 +0000

    add sidebar template and basic css styles

--- user_template_api.ps1
+++ user_template_api.ps1
@@ -559,3 +559,50 @@ Set-Location -Path '..'
 </div>
 
 {% endblock sidebar %}
+'@ | Out-File -FilePath .\users\templates\sidebar.html -Encoding utf8
+
+@'
+from django import urls
+from django.contrib.auth import views, login as auth_login
+from django.views import generic
+
+from .models import ApplicationUser as User
+
+
+# Create your views here.
+class UserList(generic.ListView):
+    queryset = User.objects.all()
+    template_name = 'index.html'
+    context_object_name = 'user_list'
+
+
+class UserDetail(generic.DetailView):
+    model = User
+    template_name = 'profile.html'
+
+
+class UserCreate(generic.CreateView):
+    model = User
+    template_name = 'create.html'
+    fields = ('email', 'password')
+
+
+class UserLogin(views.LoginView):
+    template_name = 'login.html'
+
+    # def form_valid(self, form):
+    #     auth_login(self.request, form.get_user())
+    #     return urls.reverse('user_detail', kwargs={'pk': form.get_user().pk})
+
+
+class UserLogout(views.LogoutView):
+    pass
+'@ | Out-File .\users\views.py -Encoding utf8
+
+New-Item 'static' -ItemType Directory -ea 0
+
+@'
+/*body {*/
+/*    color: red;*/
+/*}*/
+'@ | Out-File .\static\styles.css -Encoding utf8
